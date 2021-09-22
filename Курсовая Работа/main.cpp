40a164c9d6af4b92190ac5e7c53219901d78f734
Felix Titov <56549889+filtitov2001@users.noreply.github.com>
2021-09-22 22:12:10 +0300

    Update main.cpp

--- Курсовая Работа/main.cpp
+++ Курсовая Работа/main.cpp
@@ -1,9 +1,8 @@
 
 #include "cl_application.h"
 
-int main()
-{
-	cl_application ob_application(NULL); // объявление объекта приложение
-	ob_application.build_tree(); // построение исходного дерева объектов
-	return ob_application.exec_app(); // запуск функционирования системы
+int main() {
+	cl_application ob_application(NULL); // Declaration of application object
+	ob_application.build_tree(); // построение исходного дерева объектов building object's tree
+	return ob_application.exec_app(); // Starting the system functioning
 }
