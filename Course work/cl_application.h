66ded186ab9453b79c5edd14fb6a27aa7d80e455
Felix Titov <56549889+filtitov2001@users.noreply.github.com>
2021-09-22 23:17:43 +0300

    Code review

--- Course work/cl_application.h
+++ Course work/cl_application.h
@@ -11,11 +11,14 @@ class cl_application :public Base {
 	public:
 		cl_application(Base* parent, string name);
 		cl_application(Base* parent);
+	
 		void getTask(string task);
+	
 		void signal1(string& task);
 		void signal2(string& task);
 		void signal3(string& task);
 		void signal4(string& task);
+	
 		void build_tree();
 		int exec_app();
 };
