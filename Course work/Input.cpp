a3c241e682d17d2d2046c5663b5108ad0b685f2e
Felix Titov <56549889+filtitov2001@users.noreply.github.com>
2021-09-22 23:29:10 +0300

    Code review

--- Course work/Input.cpp
+++ Course work/Input.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+
 #include "Input.h"
+
 using namespace std;
-Input::Input(Base* parent, string name) :Base(parent, name)
-{
+
+Input::Input(Base* parent, string name): Base(parent, name) {
 	cl_num = 2;
 }
-void Input::signal1(string& task)// метод сигнала
-{
-}
-void Input::get(string task) // метод считывания введенной строки
-{
+
+void Input::signal1(string& task) {} // signal method
+
+void Input::get(string task) { // Method of reading the input line
 	getline(cin, task);
 	this->emitSignal(SIGNAL_D(Input, signal1), task);
-}
+}
