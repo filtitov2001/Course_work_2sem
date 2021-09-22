636d5ed0c444c45a9adb6a35b3432d103ae350e0
Felix Titov <56549889+filtitov2001@users.noreply.github.com>
2021-09-23 00:03:19 +0300

    Code review

--- Course work/Base.cpp
+++ Course work/Base.cpp
@@ -1,70 +1,65 @@
 #include <iostream>
 #include <vector>
+
 #include "Base.h"
+
 using namespace std;
-Base::Base(Base* parent) //парметризированный конструктор, определ€ющий родител€ и присваивающий ему ребенка
-{
+
+Base::Base(Base* parent) { // pairametrized constructor that defines a parent and assigns a child to it
 	this->parent = parent;
-	if (parent != NULL)
-	{
+	if (parent != NULL) {
 		parent->children.push_back(this);
 	}
 }
-Base::Base(Base* parent, string name)//парметризированный конструктор, определ€ющий родител€, им€ и присваивающий ему ребенка
-{
- this->parent = parent;
- this->name = name;
- if (parent != NULL)
- {
- parent->children.push_back(this);
- }
+
+Base::Base(Base* parent, string name) { // pairametrized constructor that defines a parent and assigns a child to it
+	this->parent = parent;
+	this->name = name;
+	if (parent != NULL) {
+	 	parent->children.push_back(this);
+	}
 }
-void Base::setStatus(int activity) // метод определени€ готовности объекта
-{
+
+void Base::setStatus(int activity) { // Method for detecting object fit
 	this->activity = activity;
 }
-void Base::printChildren() // метод вывода состо€ни€ готовности объектов
-{
+
+void Base::printChildren() { // The method of adjusting the condition of the objects
 	cout << "The object " << this->getName().c_str();
-	if (activity > 0)
-	{
+	
+	if (activity > 0) {
 		cout << " is ready";
-	}
-	else
-	{
+	} else {
 		cout << " is not ready";
 	}
-	for (int i = 0; i < children.size(); i++)
-	{
+	
+	for (int i = 0; i < children.size(); i++) {
 		cout << endl;
 		children[i]->printChildren();
 	}
 }
-void Base::inputName(string name) //метод установки имени
-{
+
+void Base::inputName(string name) { // Method for setting name
 	this->name = name;
 }
-void Base::printTree(string space) // метод вывода дерева иерархии объектов
-{
+
+void Base::printTree(string space) { // method of outputting the tree of the hierarchy of objects
 	cout << space << this->getName().c_str();
+	
 	space += " ";
-	for (int i = 0; i < children.size(); i++)
-	{
+	for (int i = 0; i < children.size(); i++) {
 		cout << endl;
 		children[i]->printTree(space);
 	}
 }
-Base* Base::searchParent(string name) // метод поиска объекта по имени
-{
+
+Base* Base::searchParent(string name) { // method of searching for the object by name
 	Base* search = NULL;
-	if (name == this->getName().c_str())
-	{
+	
+	if (name == this->getName().c_str()) {
 		search = this;
-	}
-	else if (children.size() > 0)
-	{
-		for (int i = 0; i < children.size(); i++)
-		{
+	} else if (children.size() > 0) {
+		for (int i = 0; i < children.size(); i++) {
 			search = children[i]->searchParent(name);
 			if (search != NULL)
 				break;
@@ -72,27 +67,25 @@ Base* Base::searchParent(string name) // метод поиска объекта по имени
 	}
 	return search;
 }
-Base::~Base() //деструктор, уничтожающий объекторв потовков и соединений
-{
-	for (int i = 0; i < children.size(); i++)
-	{
+
+Base::~Base() { // destructor, function of objects of streaming and connections
+	for (int i = 0; i < children.size(); i++) {
 		delete children[i];
 	}
-	for (int i = 0; i < connects.size(); i++)
-	{
+	for (int i = 0; i < connects.size(); i++) {
 		delete connects[i];
 	}
 }
-string Base::getName() // метод получени€ имени объекта
-{
+
+string Base::getName() { // Method for getting name
 	return name;
 }
-string Base::getTrailItem(string objectPath, int index_level) // метод получени€ объкта по пути с помощью индекса
-{
+
+string Base::getTrailItem(string objectPath, int index_level) { // The method of obtaining an object along the way using the index
 	int index_start, index_end, index_merchantLevel;
 	index_start = index_merchantLevel = 1;
-	while (index_start)
-	{
+	
+	while (index_start) {
 		index_end = objectPath.find("/", index_start);
 		if (index_merchantLevel == index_level)
 			return objectPath.substr(index_start, index_end -
@@ -102,26 +95,29 @@ string Base::getTrailItem(string objectPath, int index_level) // метод получени€
 	}
 	return "";
 }
-Base* Base::getObjectRoot() // метод поиска корневого объекта
-{
+
+Base* Base::getObjectRoot() { // method of searching for a root object
 	Base* MerchantBase = this;
-	while (MerchantBase->parent)
-	{
+	
+	while (MerchantBase->parent) {
 		MerchantBase = MerchantBase->parent;
 	}
 	return MerchantBase;
 }
-Base* Base::getTrailObjectRoot(string objectTrail) //метод получени€ пути корневого объекта
-{
+
+Base* Base::getTrailObjectRoot(string objectTrail) { // The method of obtaining the path of the root object
 	int index_level = 1;
 	string TrailItem;
+	
 	Base* MerchantBase = getObjectRoot();
+	
 	if (getTrailItem(objectTrail, index_level) != MerchantBase->name)
 		return NULL;
+	
 	index_level++;
 	TrailItem = getTrailItem(objectTrail, index_level);
-	while (!TrailItem.empty())
-	{
+	
+	while (!TrailItem.empty()) {
 		MerchantBase = MerchantBase->getChild(TrailItem);
 		if (!MerchantBase)
 			return NULL;
@@ -130,67 +126,62 @@ Base* Base::getTrailObjectRoot(string objectTrail) //метод получени€ пути корнев
 	}
 	return MerchantBase;
 }
-Base* Base::getObject(string objectPath) // метод поучени€ объекта дл€ последующего вывода пути
-{
+
+Base* Base::getObject(string objectPath) { // Method of drawing an object for subsequent input of a path
 	if ((objectPath[0] == '/') && (objectPath[1] == '/'))
 		return searchParent(objectPath.substr(2, string::npos));
 	return getTrailObjectRoot(objectPath);
 }
-Base* Base::getChild(string nameChild) //метод получени€ ребенка по имени
-{
-	for (int i = 0; i < children.size(); i++)
-	{
+
+Base* Base::getChild(string nameChild) { // Method of receiving the child by name
+	for (int i = 0; i < children.size(); i++) {
 		if (children[i]->name == nameChild)
 			return children[i];
 	}
 	return NULL;
 }
-void Base::setConnect(T_SIGNAL p_signal, Base* p_ob_hend, T_HENDLER
-	p_hendler) //метод установки соединений
-{
+
+void Base::setConnect(T_SIGNAL p_signal, Base* p_ob_hend, T_HENDLER p_hendler) { // Connection method
 	o_sh* value;
-	for (int i = 0; i < connects.size(); i++)
-	{
+	for (int i = 0; i < connects.size(); i++) {
 		if (connects[i]->p_signal == p_signal && connects[i] -> p_Base == p_ob_hend
 			&& connects[i]->p_hendler == p_hendler)
 			return;
 	}
+	
 	value = new o_sh();
 	value->p_Base = p_ob_hend;
 	value->p_signal = p_signal;
 	value->p_hendler = p_hendler;
 	connects.push_back(value);
+	
 }
-void Base::deleteConnect(T_SIGNAL p_signal, Base* p_ob_hend, T_HENDLER
-	p_hendler) // метод удалени€ соединений
-{
+
+void Base::deleteConnect(T_SIGNAL p_signal, Base* p_ob_hend, T_HENDLER p_hendler) { // Method for removing connections
 	o_sh* value;
-	for (int i = 0; i < connects.size(); i++)
-	{
+	for (int i = 0; i < connects.size(); i++) {
 		value = connects[i];
 		if (connects[i]->p_signal == p_signal && connects[i] -> p_Base == p_ob_hend
-			&& connects[i]->p_hendler == p_hendler)
-		{
+			&& connects[i]->p_hendler == p_hendler) {
 			delete connects[i];
 			connects.erase(connects.begin() + i);
 			return;
 		}
 	}
 }
-void Base::emitSignal(T_SIGNAL p_signal, string& command) // метод передачи сигнала
-{
+
+void Base::emitSignal(T_SIGNAL p_signal, string& command) { // Signal transmission method
 	T_HENDLER hendler;
 	(this->*p_signal)(command);
-	for (int i = 0; i < connects.size(); i++)
-	{
-		if (connects[i]->p_signal == p_signal)
-		{
+	
+	for (int i = 0; i < connects.size(); i++) {
+		if (connects[i]->p_signal == p_signal) {
 			hendler = connects[i]->p_hendler;
 			(connects[i]->p_Base->*hendler)(command);
 		}
 	}
 }
-int Base::getClassNumber() // возврат номера класса, которому принадлежит используемый объект
-{
+
+int Base::getClassNumber() { // Return the class number to which the used object belongs
 	return cl_num;
 }
