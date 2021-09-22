15081bfbbfc7c9c8b4f5d29016fe098a683a82e7
Felix Titov <56549889+filtitov2001@users.noreply.github.com>
2021-09-22 23:26:34 +0300

    Code review

--- Course work/Input.h
+++ Course work/Input.h
@@ -1,11 +1,14 @@
 #ifndef __INPUT_H
 #define __INPUT_H
+
 #include "Base.h"
-class Input : public Base
-{
+
+class Input: public Base {
+	
 	public:
 		Input(Base* parent, string name);
-		//Input(Base* parent);
+		Input(Base* parent);
+	
 		void get(string task);
 		void signal1(string& task);
 };
