346f19a27661cb7d8856cdc7ae3315213b659654
Felix Titov <56549889+filtitov2001@users.noreply.github.com>
2021-09-22 23:19:08 +0300

    Code review

--- Course work/Output.h
+++ Course work/Output.h
@@ -1,15 +1,15 @@
 #ifndef _OUTPUT_H
 #define _OUTPUT_H
+
 #include "Base.h"
 
-class Output : public Base
-{
+class Output: public Base {
 	private:
-		//string res;
 		bool first = true;
 	public:
-		//Output(Base* parent);
+		Output(Base* parent);
 		Output(Base* parent, string name);
+	
 		void print(string task);
 };
 #endif
